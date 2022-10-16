a6cc2174a89a7777dd05bb8c41311bdef896f2fa
Martin Pressl <99990993+martpres@users.noreply.github.com>
2022-10-16 19:54:06 +0200

    Initial commit

---
+++ src/main/java/fh/swm/dasletzte/githubapi/GithubapiApplication.java
@@ -0,0 +1,13 @@
+package fh.swm.dasletzte.githubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubapiApplication.class, args);
+	}
+
+}
