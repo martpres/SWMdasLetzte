ec222bf6ff6c3c05a9c6945110d05e6f5d4cb709
Martin Pressl <99990993+martpres@users.noreply.github.com>
2022-11-10 15:42:11 +0100

    from new martin-branch

--- src/main/java/fh/swm/dasletzte/githubapi/GithubapiApplication.java
+++ src/main/java/fh/swm/dasletzte/githubapi/GithubapiApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GithubapiApplication {
-private String test;
+private String  test;
 private String  test2;
+private String  test3;
     public static void main(String[] args) {
         SpringApplication.run(GithubapiApplication.class, args);
     }
