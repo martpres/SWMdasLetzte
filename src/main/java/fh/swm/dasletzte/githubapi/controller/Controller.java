4b671067e086a9fb074cf77fda0d455d59fb4b20
Martin Pressl <99990993+martpres@users.noreply.github.com>
2022-12-22 11:52:13 +0100

    add .gitattributes file

--- src/main/java/fh/swm/dasletzte/githubapi/controller/Controller.java
+++ src/main/java/fh/swm/dasletzte/githubapi/controller/Controller.java
@@ -1,4 +1,4 @@
-package controller;
+package fh.swm.dasletzte.githubapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
